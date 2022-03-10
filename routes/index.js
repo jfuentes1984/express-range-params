ca925e531f38ae016057f06444e44057f0d553ef
jfuentes1984 <6179215@cegep-heritage.qc.ca>
2022-03-10 11:38:36 -0500

    more router examples

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/count/:from-:to', (req, res) => {
+  let from = parseInt(req.params.from);
+  let to = parseInt(req.params.to);
+
+  if (!isNaN(from) && !isNaN(to)) {
+    if (to < from) {
+      let tmp = to;
+      to = from;
+      from = tmp;
+    }
+    let out = '<ul>';
+
+    for (let i = from; i <= to; i++) {
+      out += `<li>${i}</li>`;
+    }
+    out += '</ul>';
+    res.send(out);
+  } else {
+    res.send('bad parameters');
+  }
+
+});
+
+router.get('/users/:userId/books/:bookId', function (req, res) {
+  res.send(req.params.bookId + "<br>" + req.params.userId)
+});
+
+router.get('/colours/:from-:to', function (req, res) {
+});
+
+
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Hola Mundo!' });
 });
